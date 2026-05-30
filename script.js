0b57c46e064f1205674c887f210a415dc8bbe4ad
nrhnck <nrhnck@users.noreply.github.com>
2026-05-29 20:52:08 -0400

    updated with correct syntax

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
-function updateTime(){
+function updateTime() {
     const now = new Date();
-document.getElementById("time").textContent =
-"Local Time: " + now.toLocaleTimeString();
 
+    document.getElementById("time").textContent =
+        "Local Time: " + now.toLocaleTimeString();
 }
+
+updateTime(); // run immediately
 setInterval(updateTime, 1000);
