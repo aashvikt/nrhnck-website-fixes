3e5587d2429902fb9f9baf7fa9d0e3385b02f37d
nrhnck <nrhnck@users.noreply.github.com>
2026-05-29 21:02:00 -0400

    ``

--- script.js
+++ script.js
@@ -2,8 +2,12 @@ function updateTime() {
     const now = new Date();
 
     document.getElementById("time").textContent =
-        "Local Time: " + now.toLocaleTimeString();
+        "Local Time: " + now.toLocaleTimeString([], {
+            hour: "2-digit",
+            minute: "2-digit",
+            second: "2-digit"
+        });
 }
 
-updateTime(); // run immediately
+updateTime();
 setInterval(updateTime, 1000);
